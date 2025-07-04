c1ecf947e79c7326212f57df70d776b4c6b2f5c9
Jorge Medina <jorgeandres1205@gmail.com>
2025-07-04 09:02:06 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# This a Repo for the Github Demo 
+
+
---
+++ demo.py
@@ -0,0 +1,12 @@
+
+
+
+def sum(a, b):
+    """Returns the sum of two numbers."""
+    return a + b
+
+
+if __name__ == '__main__':
+    print("This is a demo script.")    
+    result = sum(3, 5)
+    print(f"The sum of 3 and 5 is: {result}")
